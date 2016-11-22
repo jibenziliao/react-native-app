314333a2c6cd0cbd3fe54084cfa546d1efb2db17
jibenziliao <1501718947@qq.com>
2016-11-22 20:26:31 +0800

    新增广场页面。

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -8,7 +8,10 @@ import {
   StyleSheet,
   Text,
   View,
-  InteractionManager
+  InteractionManager,
+  ListView,
+  RefreshControl,
+  Image
 } from 'react-native';
 import {getNavigator} from '../navigation/Route'
 import BaseComponent from '../base/BaseComponent'
@@ -18,67 +21,92 @@ import Login from '../pages/Login'
 import {Button as NBButton} from 'native-base'
 import {StyleConfig, CommonStyles, componentStyles} from '../style'
 import RNPicker from 'react-native-picker'
+import BackgroundTimer from 'react-native-background-timer'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E2E2E2'
+  },
+  contentTitle: {
+    margin: 10
+  },
+  content: {
+    flex: 1
+  },
+  card: {
+    padding: 10
+  },
+  cardRow: {
+    flexDirection: 'row'
+  },
+  cardLeft: {
+    flexDirection: 'row',
+  },
+  avatarImg: {
+    width: 100,
+    height: 100
+  },
+  userInfo: {
+    justifyContent: 'space-between'
+  },
+  userInfoLabel:{
+
+  }
+});
+
+const listViewData = [
+  {
+    avatarUrl: 'http://oatl31bw3.bkt.clouddn.com/735510dbjw8eoo1nn6h22j20m80m8t9t.jpg',
+    userId: 110,
+    userName: '张三',
+    age: 23,
+    gender: 1,
+    text: 'hello world',
+    distance: '3.9km',
+    second: 19,
+    comment: 8,
+    read: 10
+  },
+  {
+    avatarUrl: 'http://oatl31bw3.bkt.clouddn.com/735510dbjw8eoo1nn6h22j20m80m8t9t.jpg',
+    userId: 111,
+    userName: '李四',
+    age: 23,
+    gender: 1,
+    text: 'hello world',
+    distance: '3.9km',
+    second: 19,
+    comment: 8,
+    read: 10
+  }
+];
 
 let navigator;
-
-function createDateData(){
-  let date = [];
-  for(let i=1950;i<2050;i++){
-    let month = [];
-    for(let j = 1;j<13;j++){
-      let day = [];
-      if(j === 2){
-        for(let k=1;k<29;k++){
-          day.push(k+'日');
-        }
-        //Leap day for years that are divisible by 4, such as 2000, 2004
-        if(i%4 === 0){
-          day.push(29+'日');
-        }
-      }
-      else if(j in {1:1, 3:1, 5:1, 7:1, 8:1, 10:1, 12:1}){
-        for(let k=1;k<32;k++){
-          day.push(k+'日');
-        }
-      }
-      else{
-        for(let k=1;k<31;k++){
-          day.push(k+'日');
-        }
-      }
-      let _month = {};
-      _month[j+'月'] = day;
-      month.push(_month);
-    }
-    let _date = {};
-    _date[i+'年'] = month;
-    date.push(_date);
-  }
-  return date;
-}
 
 class Home extends BaseComponent {
   constructor(props) {
     super(props);
     navigator = this.props.navigator;
-
+    let ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.state = {
+      dataSource: ds.cloneWithRows(listViewData),
+      refreshing: false,
+    };
   }
 
-  _showDatePicker() {
-    RNPicker.init({
-      pickerData: createDateData(),
-      selectedValue: ['2015年', '12月', '12日'],
-      onPickerConfirm: pickedValue => {
-        console.log('date', pickedValue);
-      },
-      onPickerCancel: pickedValue => {
-        console.log('date', pickedValue);
-      },
-      onPickerSelect: pickedValue => {
-        console.log('date', pickedValue);
-      }
-    });
-    RNPicker.show();
+  _onRefresh() {
+    this.setState({refreshing: true});
+    this.timer = BackgroundTimer.setTimeout(()=> {
+      this.setState({refreshing: false});
+    }, 2000);
+  }
+
+  componentWillUnmount() {
+    BackgroundTimer.clearTimeout(this.timer);
   }
 
   getNavigationBarProps() {
@@ -106,9 +134,47 @@ class Home extends BaseComponent {
     });
   }
 
+  renderRowData(rowData) {
+    return (
+      <View key={rowData.userId}
+            style={styles.card}>
+        <View style={styles.cardRow}>
+          <View style={styles.cardLeft}>
+            <Image source={rowData.avatarUrl} style={styles.avatarImg}/>
+            <View style={styles.userInfo}>
+              <Text>{rowData.userName}</Text>
+              <View>
+                <View style={styles.userInfoLabel}>
+                  <Icon name={'venus'} size={24}/>
+                  <Text>{rowData.age}{'岁'}</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+    )
+  }
+
   renderBody() {
     return (
-      <View style={{flex: 1, paddingHorizontal: 10}}>
+      <View style={styles.container}>
+        <Text style={styles.contentTitle}>{'最新消息'}</Text>
+        <View style={styles.content}>
+          <ListView
+            refreshControl={
+              <RefreshControl
+                refreshing={this.state.refreshing}
+                onRefresh={this._onRefresh.bind(this)}
+              />
+            }
+            dataSource={this.state.dataSource}
+            renderRow={
+              this.renderRowData.bind(this)
+            }
+            initialListSize={3}
+            pageSize={3}/>
+        </View>
         <NBButton
           block
           style={{
@@ -130,17 +196,6 @@ class Home extends BaseComponent {
             this.goLogin()
           }}>
           测试登录
-        </NBButton>
-        <NBButton
-          block
-          style={{
-            height: 40,
-            marginBottom:30
-          }}
-          onPress={()=> {
-            this._showDatePicker()
-          }}>
-          测试日期选择
         </NBButton>
       </View>
     )
